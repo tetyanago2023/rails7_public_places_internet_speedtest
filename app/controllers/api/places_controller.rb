3b9f172f60a50f6b2143c9d2fddb0dd57331767e
go2023 <tetyanago2018@gmail.com>
2023-12-15 19:12:20 -0800

    PlacesList component was created

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -5,20 +5,20 @@ module Api
         {
           name: place.name,
           city: place.city,
-          most_recent__download_speed: most_recent__download_speed(place),
-          most_recent_download_speed_unit: most_recent_download_speed_unit(place),
+          most_recent_download_speed: most_recent_download_speed(place),
+          most_recent_download_speed_units: most_recent_download_speed_units(place),
           number_measurements: number_measurements(place)
         }
         end
         render(json: { places: places })
     end
 
-    def most_recent__download_speed(place)
+    def most_recent_download_speed(place)
       #Assume that all units are the same
       place.internet_speeds.order("created_at").last.download_speed
     end
 
-    def most_recent_download_speed_unit(place)
+    def most_recent_download_speed_units(place)
       place.internet_speeds.order("created_at").last.download_units
     end
 
--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 
-class PlaceList extends React.Component {
-    render() {
-        return (
-            <div>Place list rendered in React!</div>
-        )
+
+function PlacesList () {
+    const [loading, setLoading] = useState(true);
+    const [loadedPlaces, setLoadedPlaces] = useState([]);
+
+    const loadingSection = (<div>Loading...</div>)
+    const dataSection = (
+        <div>
+            <table>
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>City</th>
+                    <th>Recent Upload Speed</th>
+                    <th>Recent Upload Speed Units</th>
+                    <th>Number of Measurements</th>
+                </tr>
+                </thead>
+                <tbody>
+                {loadedPlaces.map((place, index) => (
+                    <tr key={index}>
+                        <td>{place.name}</td>
+                        <td>{place.city}</td>
+                        <td>{place.most_recent_download_speed}</td>
+                        <td>{place.most_recent_download_speed_unit}</td>
+                        <td>{place.number_measurements}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    )
+    if (loading) {
+        return loadingSection
+    } else {
+        return dataSection
     }
 }
 
 const placesList = ReactDOM.createRoot(document.getElementById("place-list-container"))
-placesList.render(<PlaceList />)
+placesList.render(<PlacesList />)
 
 
 
