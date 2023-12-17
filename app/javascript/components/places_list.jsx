54cb61c0189ae8a855af85be4214f10c70ae3140
go2023 <tetyanago2018@gmail.com>
2023-12-16 23:41:45 -0800

    tailwind classes were implemented to the placeList table

--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -26,24 +26,24 @@ function PlacesList () {
     console.log(loadedPlaces)
     const dataSection = (
         <div>
-            <table>
+            <table className={"mx-auto table-auto"}>
                 <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>City</th>
-                    <th>Recent Upload Speed</th>
-                    <th>Recent Upload Speed Units</th>
-                    <th>Number of Measurements</th>
+                <tr className={"bg-gradient-to-r from-indigo-600 to-purple-600"}>
+                    <th className={"px-16 py-2"}><span className="text-gray-100 font-semibold">Name</span></th>
+                    <th className={"px-16 py-2"}><span className="text-gray-100 font-semibold">City</span></th>
+                    <th className={"px-16 py-2"}><span className="text-gray-100 font-semibold">Recent Upload Speed</span></th>
+                    <th className={"px-16 py-2"}><span className="text-gray-100 font-semibold">Recent Upload Speed Units</span></th>
+                    <th className={"px-16 py-2"}><span className="text-gray-100 font-semibold">Number of Measurements</span></th>
                 </tr>
                 </thead>
-                <tbody>
+                <tbody className={"bg-gray-200"}>
                 {loadedPlaces.map((place, index) => (
-                    <tr key={index}>
-                        <td>{place.name}</td>
-                        <td>{place.city}</td>
-                        <td>{place.most_recent_download_speed}</td>
-                        <td>{place.most_recent_download_speed_unit}</td>
-                        <td>{place.number_measurements}</td>
+                    <tr className={"bg-white border-b-2 border-gray-200"} key={index}>
+                        <td className={"px-16 py-2"}>{place.name}</td>
+                        <td className={"px-16 py-2"}>{place.city}</td>
+                        <td className={"px-16 py-2"}>{place.most_recent_download_speed}</td>
+                        <td className={"px-16 py-2"}>{place.most_recent_download_speed_unit}</td>
+                        <td className={"px-16 py-2"}>{place.number_measurements}</td>
                     </tr>
                 ))}
                 </tbody>
